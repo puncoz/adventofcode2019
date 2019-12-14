efde4df81523d6eaa99f42e014429dde7e242a75
Puncoz Nepal <puncoz138@gmail.com>
2019-12-14 14:06:29 +0545

    day3 refactored

--- day3/soln.ts
+++ day3/soln.ts
@@ -1,79 +1,109 @@
 /**
  * CodeOfAdvent - 2019
  * Solution for Day 3
- * 
+ *
  * Problem: https://adventofcode.com/2019/day/3
- * 
+ *
  * @author Puncoz Nepal <github.com/puncoz>
  */
 
-import { readInputFile } from "../helpers"
+import { benchmark, readInputFile } from "../utils"
 
 const MAX = 999999
 const DISTANCE_MAGNITUDE = [
-    { U: 0, D: 0, R: 1, L: -1 },
-    { U: 1, D: -1, R: 0, L: 0 },
+    {U: 0, D: 0, R: 1, L: -1},
+    {U: 1, D: -1, R: 0, L: 0},
 ]
 
-const findTravelledPoints = (inputArray: string[]): Array<string> => {
-    const travelledPointsString: Array<string> = []
-    let current = [0, 0]
+const manhattanDistance = ([x, y]: [number, number]): number => {
+    return Math.abs(x) + Math.abs(y)
+}
 
-    inputArray.forEach((input: string) => {
-        const direction: string = input[0]
-        const distance: number = parseInt(input.substr(1))
+type Point = { coord: string, distance: number, steps: number }
+
+const travelledPoints = (line: string[]): Array<Point> => {
+    const travelledPoints: Array<Point> = []
+    let currentPoint = [0, 0]
+    let steps = 0
+
+    line.forEach((path: string) => {
+        const direction: string = path[0]
+        const distance: number = parseInt(path.substr(1))
 
         for (let d = 1; d <= distance; d++) {
-            const x = DISTANCE_MAGNITUDE[0][direction] + current[0]
-            const y = DISTANCE_MAGNITUDE[1][direction] + current[1]
-            current = [x, y]
+            const x = DISTANCE_MAGNITUDE[0][direction] + currentPoint[0]
+            const y = DISTANCE_MAGNITUDE[1][direction] + currentPoint[1]
+            currentPoint = [x, y]
 
-            travelledPointsString.push(`${x},${y}`)
+            const point: string = `${x},${y}`
+            steps = steps + 1
+
+            travelledPoints.push({
+                coord: point,
+                distance: manhattanDistance([x, y]),
+                steps: steps,
+            })
         }
     })
 
-    return travelledPointsString
+    return travelledPoints
+}
+
+const findIntersections = (lines: [Array<Point>, Array<Point>]): Array<Point> => {
+    // Put the smallest array in the beginning
+    if (lines[0].length > lines[1].length) {
+        const temp = lines[0]
+        lines[0] = lines[1]
+        lines[1] = temp
+    }
+
+    const hashMap = new Map()
+    for (const point of lines[0]) {
+        hashMap.set(point.coord, point)
+    }
+
+    return lines[1].reduce((intersections, secondPoint) => {
+        const firstPoint = hashMap.get(secondPoint.coord)
+        if (firstPoint) {
+            intersections.push({...secondPoint, steps: secondPoint.steps + firstPoint.steps})
+        }
+
+        return intersections
+    }, [])
 }
 
 export default async () => {
-    console.time("Initializing")
-    const inputString: string = await readInputFile(__dirname + "/input.txt")
-    // const inputString = `R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51 U98,R91,D20,R16,D67,R40,U7,R15,U6,R7`
-    const input = inputString.split(/[\s]/).filter((num: string) => !!num).map((data: string): string[] => data.split(/[,]/))
-    console.timeEnd("Initializing")
+    const lines: [string[], string[]] = await benchmark("Initializing", async () => {
+        const inputString: string = await readInputFile(__dirname + "/input.txt")
+        // const inputString = `R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51 U98,R91,D20,R16,D67,R40,U7,R15,U6,R7`
 
-    console.time("firstTravelledPoints")
-    const firstTravelledPoints = findTravelledPoints(input[0])
-    console.timeEnd("firstTravelledPoints")
+        return inputString.split(/[\s]/).filter((line: string) => !!line).map((line: string): string[] => line.split(/[,]/))
+    })
 
-    console.time("secondTravelledPoints")
-    const secondTravelledPoints = findTravelledPoints(input[1])
-    console.timeEnd("secondTravelledPoints")
+    const firstTravelledPoints: Array<Point> = await benchmark("firstTravelledPoints", async () => {
+        return travelledPoints(lines[0])
+    })
 
-    console.time("Finding intersection")
-    interface Result {
-        distance: number,
-        steps: number
-    }
+    const secondTravelledPoints: Array<Point> = await benchmark("secondTravelledPoints", async () => {
+        return travelledPoints(lines[1])
+    })
 
-    const result: Result = firstTravelledPoints.reduce((res: Result, point: string, firstIndex: number): Result => {
-        const secondIndex: number = secondTravelledPoints.indexOf(point)
+    const intersections: Array<Point> = await benchmark("Finding intersection", async () => {
+        return findIntersections([firstTravelledPoints, secondTravelledPoints])
+    })
 
-        if (secondIndex >= 0) {
-            const xy = point.split(",").map(Number)
-            const distance = Math.abs(xy[0]) + Math.abs(xy[1])
-            const step = firstIndex + secondIndex + 2
+    const closestDistance: number = await benchmark("closestDistance", async () => {
+        return intersections.reduce((min: number, point): number => {
+            return min < point.distance ? min : point.distance
+        }, MAX)
+    })
 
-            return {
-                steps: res.steps < step ? res.steps : step,
-                distance: res.distance < distance ? res.distance : distance
-            }
-        }
+    const closestSteps: number = await benchmark("closestDistance", async () => {
+        return intersections.reduce((min: number, point): number => {
+            return min < point.steps ? min : point.steps
+        }, MAX)
+    })
 
-        return res
-    }, { distance: MAX, steps: MAX })
-    console.timeEnd("Finding intersection")
-
-    console.log(`Part I: Manhattan distance to a closest intersection = ${result.distance}`)
-    console.log(`Part II: Fewest combined steps the wires must take to reach an intersection = ${result.steps}`)
+    console.log(`Part I: ${closestDistance}`)
+    console.log(`Part I: ${closestSteps}`)
 }
